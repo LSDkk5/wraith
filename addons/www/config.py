8cbd8d5e733855ff6a9ea13d26aaf6efc646514c
LSD <piotrek.m@mail.com>
2019-10-24 09:26:17 +0200

    add login method/database models

--- addons/www/config.py
+++ addons/www/config.py
@@ -3,6 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'asdasdsafsdfsdfsdfsdf23423423'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- addons/www/web.py
+++ addons/www/web.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
-from flask_login import LoginManager
+from flask import Flask, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, UserMixin
+from flask_bcrypt import Bcrypt
 
 from forms.LoginForm import LoginForm
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 login_manager = LoginManager()
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-@app.route('/')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+@app.route('/', methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(form.username.data, user.password):
+            login_user(user, remember=form.remember_me.data)
+            flash('You have been logged in!', 'success')
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
+            return redirect(url_for('login'))
     return render_template('login.html', form=form)
 
 if __name__ == "__main__":
